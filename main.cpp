b18202a0cb728f4ade806180b23fbdb443172833
Hercier <shiz20@mails.tsinghua.edu.cn>
2021-07-25 18:31:17 +0800

    add main.cpp for project1

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "btree_map.h"
+#include<iostream>
+tlx::btree_map<int,int> data;
+int main(){
+    int n,m,p,q;
+    std::cin>>n>>m>>p>>q;
+    for(int i=1;i<=n;++i){
+        int k,v;
+        std::cin>>k>>v;
+        data.insert2(k,v);
+    }
+    for(int i=1;i<=m;++i){
+        int k;
+        std::cin>>k;
+        auto it=data.find(k);
+        if(it==data.end()){
+            std::cout<<"NOT FOUND"<<std::endl;
+        }
+        else {
+            std::cout<<it->second<<std::endl;
+        }
+    }
+    for(int i=1;i<=p;++i){
+        int k,v;
+        std::cin>>k>>v;
+        data[k]=v;
+    }
+    for(int i=1;i<=q;++i){
+        int l,r;
+        std::cin>>l>>r;
+        int res=0;
+        for(int i=l;i<=r;++i){
+            res+=data.exists(i);
+        }
+        std::cout<<res<<std::endl;
+    }
+}
